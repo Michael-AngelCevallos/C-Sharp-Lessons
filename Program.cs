fd89e2ef6381525a809e5817b427045a9c50aa47
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-09 18:56:49 -0500

    First practice

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace C_Sharp_Lessons
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Mike Cevallos!");    
+        }
+    }
+}
+
