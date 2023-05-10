b225a2e875b3ca55d5d2b7803b67d8b3d67733f2
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-09 19:23:10 -0500

    finished Console Escape Sequences

--- Program.cs
+++ Program.cs
@@ -6,9 +6,37 @@ namespace C_Sharp_Lessons
     {
         static void Main()
         {
+            // Console Log use Console.WriteLine()
+            Console.WriteLine("I like pizza");
+            
+            
+            // Console Input 
             Console.WriteLine("How Old Are You?");
             string input = Console.ReadLine();
             Console.WriteLine($"You are {input} years old!");
+            
+            
+            // How To Format Output
+            Console.Write("Hey"); // Console.Write will not hit enter key after declaration
+            Console.WriteLine("Hey is on the same line as this one"); // Console.Writeline Will Hit Enter key after Declaration
+            
+            // Auto generate Console.Writeline ( c + w + tab)
+            Console.WriteLine();
+            
+            // Console Escape Sequences 
+            Console.WriteLine("\tMike Cevallos"); // returns Mike Cevallos, Tabbed in (\t)
+
+            Console.WriteLine("Mike\bCevallos"); // returns MikCevallos, uses backspace(\b)
+
+            Console.WriteLine("Mike\nCevallos"); // returns Mike (new line) Cevallos
+            
+            
+
+
+
+
+
+
         }
     }  
         }
